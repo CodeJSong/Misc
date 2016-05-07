e198d4e555edaba7e2e0bd70559b14356265d5cb
Jun Song <songjun0525@gmail.com>
2016-05-06 21:02:55 -0400

    Get OS information in R

---
+++ getOS.R
@@ -0,0 +1,25 @@
+# code from googling
+
+# getOS.R   : Get OS information in R
+# Sometimes, windows need to set path to Rtools for RCPP
+
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+
+if(get_os()=="windows"){
+  Sys.setenv(PATH="%PATH%;C:/Rtools/gcc-4.6.3/bin;c:/Rtools/bin")
+}
